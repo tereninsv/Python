64d9d39331af65829b6a236219baf8b9586c7a59
svterenin <svterenin@avito.ru>
2019-01-21 15:23:43 +0300

    lesson5

--- lesson3_client.py
+++ lesson3_client.py
@@ -1,7 +1,6 @@
 import socket
-import json
 import time
-from utils import dict_to_bytes, bytes_to_dict
+from utils import *
 
 
 class Client:
@@ -14,20 +13,23 @@ class Client:
     def authenticate(self):
         server.send(dict_to_bytes({"action": "authenticate", "time": time.time(),
                                    "user": {"account_name": self.account_name, "password": self.password}}))
+        log_info(log, "authenticate")
 
     def presence(self, status):
         server.send(dict_to_bytes({"action": "presence", "time": time.time(),
                                    "user": {"account_name": self.account_name, "status": status}}))
+        log_info(log, "presence")
 
     def msg(self):
         to = input('кому:')
         message = input('сообщение:')
         server.send(dict_to_bytes({"action": "msg", "time": time.time(),
                                    "to": to, "from": self.account_name, "encoding": "ascii", "message": message}))
-
+        log_info(log, "msg")
     def quit(self):
         server.send(dict_to_bytes({"action": "quit", "time": time.time(),
                                    "account_name": self.account_name}))
+        log_info(log, "quit")
         server.close()
         exit(0)
 
@@ -36,28 +38,40 @@ class Client:
 
     def response(self):
         server_response = bytes_to_dict(server.recv).get("response")
+        log_info(log, server_response)
         print(server_response)
         return server_response
 
+def server_connect():
+    try:
+        server = socket.socket()
+        server.connect(('localhost', 7777))
+    except:
+        log_critical(log_client, 'not connect')
+    return server
+
 
 if __name__ == "__main__":
-    server = socket.socket()
-    server.connect(('localhost', 7777))
+    # log_client = start_log('client')
+    server = server_connect()
+
 
     server_response = ''
+    # log_debug(log_client, 'start auth')
     while server_response != '200':
         client = Client(input('введите логин:'), input('введите пароль:'))
+        log = start_log(client.account_name)
         client.authenticate()
         server_response = client.response()
 
-chat = {
-    "msg": client.msg,
-    "quit": client.quit
-}
+    chat = {
+        "msg": client.msg,
+        "quit": client.quit
+    }
 
-while True:
-    act = input("введите действие:")
-    if chat.get(act):
-        chat[act]()
-    else:
-        print('такой команды не существует')
+    while True:
+        act = input("введите действие:")
+        if chat.get(act):
+            chat[act]()
+        else:
+            print('такой команды не существует')
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import json
+import logging
+from logging.handlers import RotatingFileHandler
 from settings import *
 
 
@@ -8,3 +10,41 @@ def dict_to_bytes(data):
 
 def bytes_to_dict(data):
     return json.loads(data(BUF_SIZE).decode(ENCODING))
+
+def start_serverlog(path):
+    logging.basicConfig(
+        filename='server.log',
+        format='%(levelname)-10s %(asctime)s %(message)s',
+        level=logging.DEBUG
+    )
+    log = logging.getLogger('basic')
+    log.debug('start log')
+    handler = RotatingFileHandler(path, maxBytes=20)
+    log.addHandler(handler)
+
+    return log
+
+
+
+def start_log(logname):
+    logging.basicConfig(
+        filename=logname + '.log',
+        format='%(levelname)-10s %(asctime)s %(message)s',
+        level=logging.DEBUG
+    )
+    log = logging.getLogger('basic')
+    log.debug('start log')
+    return log
+
+def log_debug(log,msg):
+    return log.debug(msg)
+
+def log_info(log,msg):
+    return log.info(msg)
+
+def log_warning(log,msg):
+    return log.warning(msg)
+
+def log_critical(log,msg):
+    return log.critical(msg)
+
